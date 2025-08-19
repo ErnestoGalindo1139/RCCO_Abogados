a245c26dbec601ecd641d4c9ea47fa39848644ca
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2025-08-18 21:12:13 -0700

    Cambios del banner

--- src/TiendaOnline/components/Banner.tsx
+++ src/TiendaOnline/components/Banner.tsx
@@ -14,10 +14,10 @@ export const Banner = () => {
         <div className="absolute inset-0 bg-blue-900 opacity-85"></div>
         
         {/* Puedes añadir contenido aquí para que aparezca sobre la imagen y el fondo */}
-        <div className="absolute inset-0 flex flex-col items-start justify-center text-center px-12">
-          <h3 className="text-white text-8xl font-bold">Comprometidos con</h3>
-          <h3 className="text-white text-8xl font-bold">tu tranquilidad legal</h3>
-          <button className='bg-white text-blue-900 font-semibold h-14 px-2 rounded-lg text-2xl'>Agenda una cita</button>
+        <div className="absolute inset-0 flex flex-col items-start justify-center text-center px-20">
+          <h3 className="text-white text-8xl font-bold">Ingeniería jurídica</h3>
+          <h3 className="text-white text-8xl font-bold">para tu empresa</h3>
+          <button className='bg-white text-blue-900 font-semibold h-14 px-2 rounded-lg text-2xl mt-9'>Agenda una cita</button>
         </div>
       </div>
 
